e2dbc62558bd6fdf28fc34a8976387bea1a5d449
Sirirat Kongdee <siriratk@amazon.com>
2024-02-21 02:09:40 +0000

    add log group

--- PetAdoptions/cdk/pet_stack/lib/observability.ts
+++ PetAdoptions/cdk/pet_stack/lib/observability.ts
@@ -5,11 +5,17 @@ import { RUM } from './modules/rum';
 import * as fs from 'fs';
 import { Alarm, ComparisonOperator, TreatMissingData, Metric } from 'aws-cdk-lib/aws-cloudwatch'
 import { Alias } from 'aws-cdk-lib/aws-kms';
-import * as cr from 'aws-cdk-lib/custom-resources'
+import * as cr from 'aws-cdk-lib/custom-resources';
+import * as logs from 'aws-cdk-lib/aws-logs';
 
 export class Observability extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    const fisLogGroup = new logs.LogGroup(this, 'FISLogGroup', {
+      logGroupName: 'FISExperiments',
+    });
+    
     
     const petSiteUrl = ssm.StringParameter.valueFromLookup(this, '/petstore/petsitedomain');
     //const domain: string = ssm.StringParameter.fromStringParameterAttributes(this, 'getParamPetSiteDomain', { parameterName: "/petstore/petsitedomain"}).stringValue;
@@ -60,7 +66,6 @@ export class Observability extends cdk.Stack {
       }),
     })
 
-    //new ssm.StringParameter(this, '/petstore/rumscript', { parameterName: '/petstore/rumscript', stringValue: rum.htmlScript });
 
     // Alarm for Frustrating navigation experience
     const navigationFrustratedMetric = new Metric({
