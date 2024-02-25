3721e846ad80d46b903c6f11cf94bdfe2afa0691
Laurent Domb <ldo@amazon.com>
2024-02-25 12:43:58 -0500

    updated service definition with jq

--- ecs-experiment/updatetaskdef.sh
+++ ecs-experiment/updatetaskdef.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-ECS_CLUSTER_ARN=$(aws ecs list-clusters | grep PetSearch | tr -d '"')
+#!/bin/bash
+ECS_CLUSTER_ARN=$(aws ecs list-clusters | jq -r '.clusterArns[] | select(contains("PetSearch"))')
 echo $ECS_CLUSTER_ARN
-TASK_DEFS=$(aws ecs list-task-definitions | grep search | tr -d ',' | tr -d '"')
+TASK_DEFS=$(aws ecs list-task-definitions | jq -r '.taskDefinitionArns[] | select(contains("search"))')
 echo $TASK_DEFS
 ECS_SRV_ARN=$(aws ecs list-services --cluster $ECS_CLUSTER_ARN | jq -r  '.serviceArns[0]') 
 echo $ECS_SRV
