6476929a6c54c5b3036e2c1d2e35d8ed997a8852
Laurent Domb <ldo@amazon.com>
2024-02-27 12:01:50 -0500

    fixed script to point to the right yaml

--- api-actions-experiment/run-api-throttling-setup.sh
+++ api-actions-experiment/run-api-throttling-setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 ROLEARN=$(aws iam list-roles | jq '.Roles[].Arn | select(contains("cfn-exec"))' -r)
-SUBNETID=$(aws ec2 describe-subnets --filters "Name=tag:Name,Values=Services/Microservices/PublicSubnet1" --query "Subnets[].SubnetId" --output text)
-echo $SUBNETID
 
+echo "Stack deployment takes about 1min"
 aws cloudformation create-stack --stack-name fisapithrottle --template-body file://api-throttling.yaml --parameters ParameterKey=LambdaFunctionName,ParameterValue=fis-workshop-api-errors-throttling ParameterKey=apiGatewayName,ParameterValue=fis-workshop-throttle ParameterKey=apiGatewayStageName,ParameterValue=v1 --capabilities CAPABILITY_NAMED_IAM --role-arn $ROLEARN
 
 aws cloudformation wait stack-create-complete --stack-name fisapithrottle
