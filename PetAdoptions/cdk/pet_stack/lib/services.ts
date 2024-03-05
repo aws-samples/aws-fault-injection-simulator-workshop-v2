e5b12713fb2d4248a7642fbfb5d00fb2a280e522
Vladislav Nedosekin <mr.vladis@gmail.com>
2024-03-05 14:27:40 +0000

    tag cleanup

--- PetAdoptions/cdk/pet_stack/lib/services.ts
+++ PetAdoptions/cdk/pet_stack/lib/services.ts
@@ -474,7 +474,6 @@ export class Services extends Stack {
         // Tagging Launch Template
         // cdk.Tags.of(petsiteEKSlt).add('AzImpairmentPower', 'Ready');
         // Adding ClusterNodeGroupRole
-
         const eksPetsiteASGClusterNodeGroupRole = new iam.Role(this, 'eksPetsiteASGClusterNodeGroupRole', {
             roleName: 'eksPetsiteASGClusterNodeGroupRole',
             assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
@@ -494,7 +493,6 @@ export class Services extends Stack {
             },
             labels: {
                 ["AzImpairmentPower"]: "Ready",
-                ["WorkhopLabel"]: "Ready",
             },
             desiredSize: 2,
             maxSize: 2,
