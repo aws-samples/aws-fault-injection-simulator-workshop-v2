9ad1f81a377d3a653bec57bb459b23d545c5b0d7
Laurent Domb <ldo@amazon.com>
2024-03-14 14:01:01 -0400

    added automation to install the cwagent onpremises

--- hybrid-experiment/cw_aws_setup_for_on_prem.sh
+++ hybrid-experiment/cw_aws_setup_for_on_prem.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-BASEDIR=$(pwd)
 # Create the IAM user
 aws iam create-user --user-name cwagent
 
 # Create an access key for the user
-ACCESS_KEY=$(aws iam create-access-key --user-name cwagent --query 'AccessKey.AccessKeyId' --output text)
-SECRET_KEY=$(aws iam create-access-key --user-name cwagent --query 'AccessKey.SecretAccessKey' --output text)
+aws iam create-access-key --user-name cwagent --output json > access_key_output.json
+ACCESS_KEY_ID=$(jq -r '.AccessKey.AccessKeyId' access_key_output.json)
+SECRET_ACCESS_KEY=$(jq -r '.AccessKey.SecretAccessKey' access_key_output.json)
+rm -f access_key_output.json
 
 # Attach the AmazonSSMManagedInstanceCore policy
 aws iam attach-user-policy --user-name cwagent --policy-arn arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore
 
 # Attach the CloudWatchAgentServerPolicy policy
 aws iam attach-user-policy --user-name cwagent --policy-arn arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy
-
-# Save the access key and secret key in the credential file
-echo "$ACCESS_KEY $SECRET_KEY" 
 
 mkdir .aws
 cd .aws
@@ -26,8 +24,8 @@ EOF
 
 cat > credentials <<EOF
 [AmazonCloudWatchAgent]
-aws_access_key_id = $ACCESS_KEY
-aws_secret_access_key = $SECRET_KEY
+aws_access_key_id = $ACCESS_KEY_ID
+aws_secret_access_key = $SECRET_ACCESS_KEY
 EOF
 
 cd ..
