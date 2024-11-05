f8d20367ddc80a9c9b0380f36660de00f0ada57a
Saurabh Kumar <saurabks@amazon.com>
2024-11-05 16:54:14 -0500

    D174693944: fixing status update lambda function

--- PetAdoptions/petstatusupdater/index.js
+++ PetAdoptions/petstatusupdater/index.js
@@ -1,41 +1,35 @@
-'use strict';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  UpdateCommand,
+} from "@aws-sdk/lib-dynamodb";
 
-var AWSXRay = require('aws-xray-sdk');
-var AWS = AWSXRay.captureAWS(require('aws-sdk'));
-var documentClient = new AWS.DynamoDB.DocumentClient();
+const client = new DynamoDBClient({});
+const dynamoClient = DynamoDBDocumentClient.from(client)
 
-exports.handler = async function (event, context, callback) {
-    var payload = JSON.parse(event.body);
+export const handler = async function (event, context, callback) {
+  var payload = JSON.parse(event.body);
+  var availability = "yes";
+  if (payload.petavailability === void 0) {
+    availability = "no";
+  }
 
-    var availability = "yes";
-    if (payload.petavailability === undefined) {
-        availability = "no";
-    }
-    var params = {
-        TableName: process.env.TABLE_NAME,
-        Key: {
-            "pettype": payload.pettype,
-            "petid": payload.petid
-        },
-        UpdateExpression: "set availability = :r",
-        ExpressionAttributeValues: {
-            ":r": availability
-        }, ReturnValues: "UPDATED_NEW"
-    };
+  const command = new UpdateCommand({
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      "pettype": payload.pettype,
+      "petid": payload.petid
+    },
+    UpdateExpression: "set availability = :r",
+    ExpressionAttributeValues: {
+      ":r": availability
+    },
+    ReturnValues: "UPDATED_NEW"
+  });
 
-    await updatePetadoptionsTable(params);
+  const response = await dynamoClient.send(command);
+  console.log(response);
+  console.log("Updated petid: " + payload.petid + ", pettype: " + payload.pettype + ", to availability: " + availability);
 
-    console.log("Updated petid: " + payload.petid + ", pettype: " + payload.pettype + ", to availability: " + availability);
-    return { "statusCode": 200, "body": "success" };
+  return { "statusCode": 200, "body": "success" };
 };
-
-async function updatePetadoptionsTable(params) {
-    await documentClient.update(params, function (err, data) {
-        if (err) {
-            console.log(JSON.stringify(err, null, 2));
-        } else {
-            console.log(JSON.stringify(data, null, 2));
-            //  console.log("Updated petid: "+payload.petid +", pettype: "+payload.pettype+ " to availability: "+availability);
-        }
-    }).promise();
-}
