95a851e68144b6c1d0270e3364672fa28f62bfa0
Vladislav Nedosekin <mr.vladis@gmail.com>
2024-11-24 15:43:36 +0000

    script moved to scripts folder

--- reg-experiment/scripts/setup_regexperiment_fasttrack.sh
+++ reg-experiment/scripts/setup_regexperiment_fasttrack.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 cd ~/environment/workshopfiles/fis-workshop/reg-experiment/iam/
 aws iam create-role --role-name fis-reg-role --assume-role-policy-document file://fis-reg-experiment-policy.json
 aws iam put-role-policy --role-name fis-reg-role --policy-name fis-reg-policy --policy-document file://fis-reg-task-policy.json
