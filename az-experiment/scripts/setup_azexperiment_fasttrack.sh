89438ee68d9c3fe54a9b77c51a9903ed2c20bd8f
mrvladis <mr.vladis@gmail.com>
2024-11-26 19:21:13 +0000

    Updates

--- az-experiment/scripts/setup_azexperiment_fasttrack.sh
+++ az-experiment/scripts/setup_azexperiment_fasttrack.sh
@@ -3,7 +3,6 @@ cd ~/environment/workshopfiles/fis-workshop/az-experiment/iam/
 aws iam create-role --role-name fis-az-role --assume-role-policy-document file://fis-az-experiment-policy.json
 aws iam put-role-policy --role-name fis-az-role --policy-name fis-az-policy --policy-document file://fis-az-task-policy.json
 cd ~/environment/workshopfiles/fis-workshop/az-experiment/scripts/
-bash removeusersimtags.sh
 cd ~/environment/workshopfiles/fis-workshop/ecs-experiment/
 sh updatetaskdef.sh
 cd -
