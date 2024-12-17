3923e64f7bbd8e322cc16ef236610a8100f15ef1
Laurent Domb <github@domb.net>
2024-12-16 19:33:11 -0500

    Delete scripts/byo/runme.sh

--- scripts/byo/runme.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-aws cloudformation create-stack \
-  --stack-name my-pipeline-stack \
-  --template-body file://pipeline-template.yaml \
-  --parameters file://parameters.json \
-  --capabilities CAPABILITY_IAM
