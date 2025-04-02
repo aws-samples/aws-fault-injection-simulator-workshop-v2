a159956611786d8625ae93197ab0b4a70a467d4d
Laurent Domb <ldo@amazon.com>
2025-04-02 08:25:33 -0400

    enabling module for autoshift disabled

--- az-experiment/scripts/setup_azexperiment_fasttrack.sh
+++ az-experiment/scripts/setup_azexperiment_fasttrack.sh
@@ -1,4 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+function list_load_balancers() {
+    echo "Load Balancers:"
+    aws elbv2 describe-load-balancers --query 'LoadBalancers[*].{Name:LoadBalancerName,Type:Type}' --output table
+}
+
+
+# Main script
+
 cd ~/environment/workshopfiles/fis-workshop/az-experiment/iam/
 aws iam create-role --role-name fis-az-role --assume-role-policy-document file://fis-az-experiment-policy.json
 aws iam put-role-policy --role-name fis-az-role --policy-name fis-az-policy --policy-document file://fis-az-task-policy.json
@@ -6,3 +15,39 @@ cd ~/environment/workshopfiles/fis-workshop/az-experiment/scripts/
 cd ~/environment/workshopfiles/fis-workshop/ecs-experiment/
 sh updatetaskdef.sh
 cd -
+
+list_load_balancers
+  
+echo "Enabling zonal_shift.config.enabled true"
+eksasg=$(aws autoscaling describe-auto-scaling-groups --query "AutoScalingGroups[?contains(AutoScalingGroupName, 'eks')].AutoScalingGroupName" --output text)
+eksasg=$(echo "$eksasg" | tr -d '[:space:]')
+eksasgarn=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $eksasg --query 'AutoScalingGroups[0].AutoScalingGroupARN' --output text)
+aws autoscaling update-auto-scaling-group \
+    --auto-scaling-group-name "$eksasg" \
+    --availability-zone-impairment-policy '{"ZonalShiftEnabled": true, "ImpairedZoneHealthCheckBehavior": "IgnoreUnhealthy"}'
+echo "Check $eksasg settings for zonalshift and unhealthy hosts"
+
+aws autoscaling describe-auto-scaling-groups \
+    --auto-scaling-group-names "$eksasg" \
+    --query 'AutoScalingGroups[0].AvailabilityZoneImpairmentPolicy' 
+
+ecssrv=$(aws autoscaling describe-auto-scaling-groups --query "AutoScalingGroups[?contains(AutoScalingGroupName, 'Services')].AutoScalingGroupName" --output text)
+ecssrv=$(echo "$ecssrv" | tr -d '[:space:]')
+ecsasgarn=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $ecssrv --query 'AutoScalingGroups[0].AutoScalingGroupARN' --output text)
+aws autoscaling update-auto-scaling-group \
+    --auto-scaling-group-name "$ecssrv" \
+    --availability-zone-impairment-policy '{"ZonalShiftEnabled": true, "ImpairedZoneHealthCheckBehavior": "IgnoreUnhealthy"}'
+echo "Check $ecssrv settings for zonalshift and unhealthy hosts"
+
+aws autoscaling describe-auto-scaling-groups \
+    --auto-scaling-group-names "$ecssrv" \
+    --query 'AutoScalingGroups[0].AvailabilityZoneImpairmentPolicy' 
+
+
+load_balancer_arns=$(aws elbv2 describe-load-balancers --query 'LoadBalancers[*].LoadBalancerArn' --output text)
+
+for load_balancer_arn in $load_balancer_arns; do
+    aws elbv2 modify-load-balancer-attributes --load-balancer-arn $load_balancer_arn --attributes Key=zonal_shift.config.enabled,Value=true
+    start_zonal_shift "$load_balancer_arn" "$AZ"
+done
+
--- az-experiment/scripts/withzonalshift.sh
+++ az-experiment/scripts/withzonalshift.sh
@@ -1,33 +1,9 @@
 #!/bin/bash
 
-AZ=$1
-TIME=$2
-
 # Function to list all load balancers
 function list_load_balancers() {
     echo "Load Balancers:"
     aws elbv2 describe-load-balancers --query 'LoadBalancers[*].{Name:LoadBalancerName,Type:Type}' --output table
-}
-
-
-# Function to start the Zonal Shift
-function start_zonal_shift() {
-    # Set the AWS region
-    AWS_REGION=$AWS_REGION
-    #echo $AWS_REGION
-    #echo $AZ
-
-    # Get the Availability Zone information
-    AZ_INFO=$(aws ec2 describe-availability-zones --region $AWS_REGION --output json)
-
-    # Extract the Availability Zone ID for us-east-1a
-    AZ_ID=$(echo $AZ_INFO | jq -r '.AvailabilityZones[] | select(.ZoneName == "'"$AZ"'") | .ZoneId')
-    #echo $AZ_ID
-
-
-    # Print the Availability Zone ID
-    echo "Failing away from:  $AZ_ID"
-    aws arc-zonal-shift start-zonal-shift --away-from "$AZ_ID" --expires-in $TIME --resource-identifier "$load_balancer_arn" --comment "shift away from AZ $AZ"
 }
 
 # Function to monitor the progress of the Zonal Shift
@@ -56,19 +32,8 @@ echo $experiment
 
   if [ "$experiment" == "running" ]; then
     echo "Impact Detected!!!!!" 
-   
-
-    countdown_time=5
-    while [ $countdown_time -gt 0 ]; do
-        printf "\rTime to zonal shift: %02d:%02d" $countdown_time 0
-        sleep 1m
-        ((countdown_time--))
-    done
     
-    echo
-    echo "Enabling zonal_shift.config.enabled true"
-    
-    echo "Enabling Zonal Shift"
+    echo "Waiting for Zonal Shift to be active" 
 
    # Main script
 
@@ -78,9 +43,7 @@ echo $experiment
    eksasg=$(aws autoscaling describe-auto-scaling-groups --query "AutoScalingGroups[?contains(AutoScalingGroupName, 'eks')].AutoScalingGroupName" --output text)
    eksasg=$(echo "$eksasg" | tr -d '[:space:]')
    eksasgarn=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $eksasg --query 'AutoScalingGroups[0].AutoScalingGroupARN' --output text)
-   aws autoscaling update-auto-scaling-group \
-        --auto-scaling-group-name "$eksasg" \
-        --availability-zone-impairment-policy '{"ZonalShiftEnabled": true, "ImpairedZoneHealthCheckBehavior": "IgnoreUnhealthy"}'
+
    echo "Check $eksasg settings for zonalshift and unhealthy hosts"
 
    aws autoscaling describe-auto-scaling-groups \
@@ -90,9 +53,7 @@ echo $experiment
    ecssrv=$(aws autoscaling describe-auto-scaling-groups --query "AutoScalingGroups[?contains(AutoScalingGroupName, 'Services')].AutoScalingGroupName" --output text)
    ecssrv=$(echo "$ecssrv" | tr -d '[:space:]')
    ecsasgarn=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $ecssrv --query 'AutoScalingGroups[0].AutoScalingGroupARN' --output text)
-   aws autoscaling update-auto-scaling-group \
-        --auto-scaling-group-name "$ecssrv" \
-        --availability-zone-impairment-policy '{"ZonalShiftEnabled": true, "ImpairedZoneHealthCheckBehavior": "IgnoreUnhealthy"}'
+
    echo "Check $ecssrv settings for zonalshift and unhealthy hosts"
 
    aws autoscaling describe-auto-scaling-groups \
@@ -102,23 +63,9 @@ echo $experiment
 
    load_balancer_arns=$(aws elbv2 describe-load-balancers --query 'LoadBalancers[*].LoadBalancerArn' --output text)
 
-    for load_balancer_arn in $load_balancer_arns; do
-       aws elbv2 modify-load-balancer-attributes --load-balancer-arn $load_balancer_arn --attributes Key=zonal_shift.config.enabled,Value=true
-       start_zonal_shift "$load_balancer_arn" "$AZ"
-    done
-
     echo "EKS cluster PetSite in zonalshift mode"
     cluster_arn=$(aws eks describe-cluster --name PetSite --query 'cluster.arn' --output text)
     echo "EKS Cluster ARN:" $cluster_arn
-    aws arc-zonal-shift start-zonal-shift --away-from "$AZ_ID" --expires-in $TIME --resource-identifier "$cluster_arn" --comment "shift away from AZ $AZ"
-
-    echo "Shifting ASG:" $eksasg
-    aws arc-zonal-shift start-zonal-shift --away-from "$AZ_ID" --expires-in $TIME --resource-identifier "$eksasgarn" --comment "shift away from AZ $AZ"
-
-    echo "Shifting ASG:" $ecssrv
-    aws arc-zonal-shift start-zonal-shift --away-from "$AZ_ID" --expires-in $TIME --resource-identifier "$ecsasgarn" --comment "shift away from AZ $AZ"
-
-    sleep 10
 
     while true; do
         all_processed=true
@@ -131,18 +78,7 @@ echo $experiment
         done
 
         if $all_processed; then
-            echo "All load balancers have been processed."
-            aws autoscaling update-auto-scaling-group --auto-scaling-group-name "$eksasg" \
-                --availability-zone-impairment-policy '{
-                    "ZonalShiftEnabled": false, 
-                    "ImpairedZoneHealthCheckBehavior": "IgnoreUnhealthy"}'
-            aws autoscaling update-auto-scaling-group --auto-scaling-group-name "$ecssrv" \
-                --availability-zone-impairment-policy '{
-                    "ZonalShiftEnabled": false, 
-                    "ImpairedZoneHealthCheckBehavior": "IgnoreUnhealthy"}'
-            for load_balancer_arn in $load_balancer_arns; do
-                aws elbv2 modify-load-balancer-attributes --load-balancer-arn $load_balancer_arn --attributes Key=zonal_shift.config.enabled,Value=false
-            done
+            echo "Impact is mitiagted, Zonal Shift is being deactivated"
             break
         fi
 
