165b4cf25b3dc0d3c4d732cf5f658cb404db9880
Jason Brown <browwjas@amazon.com>
2025-11-04 11:54:35 -0800

    fix gradle dependencies v7

--- PetAdoptions/petsearch-java/src/main/java/ca/petsearch/ApplicationFilter.java
+++ PetAdoptions/petsearch-java/src/main/java/ca/petsearch/ApplicationFilter.java
@@ -2,9 +2,9 @@ package ca.petsearch;
 
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ApplicationFilter implements Filter {
