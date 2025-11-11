bff1552f0f1f85959c35dcc415285861aa4bad9a
Shishir <nshishir@amazon.com>
2025-11-11 00:05:56 -0600

    Upgrade Node.js to version 22.7.9 across all components
    - Updated @types/node to 22.7.9 in all package.json files
    - Upgraded Lambda runtime from NODEJS_16_X to NODEJS_22_X in fis_serverless.ts
    - Ran npm install to update package-lock.json files
    - Node.js 22.x supported until April 30, 2027

--- PetAdoptions/cdk/pet_stack/lib/fis_serverless.ts
+++ PetAdoptions/cdk/pet_stack/lib/fis_serverless.ts
@@ -17,7 +17,7 @@ export class FisServerless extends Stack {
         const nodeLayer = new LayerVersion(this, 'NodeChaosLayer', {
             layerVersionName: 'ChaosNodeLayer',
             compatibleRuntimes: [
-                Runtime.NODEJS_16_X
+                Runtime.NODEJS_22_X
             ],
             code: Code.fromAsset('lib/fis_serverless/layer'),
             compatibleArchitectures: [
