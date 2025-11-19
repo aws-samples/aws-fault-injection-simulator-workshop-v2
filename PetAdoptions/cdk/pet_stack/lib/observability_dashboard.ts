cdec29b0a2c479cd4881e30aa843916e60a140d1
Jason Brown <browwjas@amazon.com>
2025-11-19 02:04:58 -0800

    remove second region deployment for testing

--- PetAdoptions/cdk/pet_stack/lib/observability_dashboard.ts
+++ PetAdoptions/cdk/pet_stack/lib/observability_dashboard.ts
@@ -3,13 +3,19 @@ import { Construct } from 'constructs';
 import { ServiceStackProps } from './common/services-shared-properties';
 import { MultiRegionDashboard } from './dashboards/multi_region_dashboard';
 import { AZDashboard } from './dashboards/az_dashboard';
+import { getDeploymentConfig } from './common/deployment-config';
 
 export class ObservabilityDashboard extends cdk.Stack {
 
     constructor(scope: Construct, id: string, props: ServiceStackProps) {
         super(scope, id, props);
 
-        new MultiRegionDashboard(this, props);
+        const deploymentConfig = getDeploymentConfig();
+        
+        // Only create multi-region dashboard if multi-region is enabled
+        if (deploymentConfig.enableMultiRegion) {
+            new MultiRegionDashboard(this, props);
+        }
 
         new AZDashboard(this, props);
         
